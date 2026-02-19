9ab366994dee8d160bcba037d2c77d6a2e40e726
Marcus Lund da Almeida <mig@pc-27.home>
2026-02-19 18:13:00 +0100

    mandelbrot_point function has been added and testet

--- mandelbort.py
+++ mandelbort.py
@@ -1,10 +1,19 @@
+import numpy as np
+
 """
 Mandelbrot set generator
 By [Marcus d Almeida]
 Course: Numerical Scientific Computing 2026
 """
 
-def f(x):
-    return print(2*x)
+def mandelbrot_point(c, max_iter):
+    z = 0
+    for i in range(max_iter):
+        z = z**2 + c
+        if abs(z) > 2:
+            print(f"{i} iterations before divergence")
+            return
+    print("not enough iterations")
+    
 
-f(42)
+mandelbrot_point(2+0j,20) 
