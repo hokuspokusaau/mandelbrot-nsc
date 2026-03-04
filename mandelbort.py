322bf7642a53209848a060b9e5f7b3a445b8bf62
Marcus Lund da Almeida <mig@pc-27.home>
2026-03-04 12:46:48 +0100

    Baseline time for naive implementation at: 1024x1024 grid size, c = 0, max_iter = 100

--- mandelbort.py
+++ mandelbort.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 """
 Mandelbrot set generator
 By [Marcus d Almeida]
@@ -7,13 +7,27 @@ Course: Numerical Scientific Computing 2026
 """
 
 def mandelbrot_point(c, max_iter):
-    z = 0
+    z = 0+0j
     for i in range(max_iter):
         z = z**2 + c
         if abs(z) > 2:
-            print(f"{i} iterations before divergence")
-            return
-    print("not enough iterations")
-    
+            return i
+    return max_iter
 
-mandelbrot_point(2+0j,20) 
+def compute_mandelbrot(xmin, xmax, ymin, ymax, width, height):
+        result = np.zeros((height, width), dtype=int)
+
+        xs = np.linspace(xmin, xmax, width)
+        ys = np.linspace(ymin, ymax, height)
+
+        for i, y in enumerate(ys) :
+             for j, x in enumerate(xs):
+                  c = complex(x,y)
+                  result[i, j] = mandelbrot_point(0, 100)
+        return print(f"{result}")
+
+
+start = time.time()
+result = compute_mandelbrot(-2, 1, -1.5, 1.5, 1024, 1024,)
+elapsed = time.time() - start 
+print(f"Computation took {elapsed: .3f} seconds")
