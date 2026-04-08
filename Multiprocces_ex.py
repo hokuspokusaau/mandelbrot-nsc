6cbfbd6f4d20f2f38e5754e4a0963f1951ef4c20
Marcus Lund da Almeida <mig@pc-27.home>
2026-04-08 18:25:23 +0200

    fixed parallel timing issue

---
+++ Multiprocces_ex.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+import time
+import numpy as np
+
+def square(x):
+    time.sleep(0.1)
+    return x * x
+
+if __name__ == '__main__':
+    numbers = list(range(100))
+
+    start = time.time()
+    results_serial = [square(x) for x in numbers]
+    time_serial = time.time() - start
+    print(f"Serial: {time_serial: .2f}s")
+
+    with Pool(processes=3) as pool:
+        start = time.time()
+        results_parallel = pool.map(square, numbers)
+        time_parallel = time.time() - start
+    print(f"Parallel: {time_parallel: .2f}s")
+    speedup = time_serial / time_parallel
+    print(f"Speedup: {speedup: .2f}x")
